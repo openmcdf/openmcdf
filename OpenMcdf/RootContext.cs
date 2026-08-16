7e0c00b18aa7500cd4303130a8f36e088082e384
Jeremy Powell <jeremy@visionaid.com>
2026-08-17 10:33:40 +1200

    Flush dirty FAT when trimming past range lock

--- OpenMcdf/RootContext.cs
+++ OpenMcdf/RootContext.cs
@@ -216,7 +216,10 @@ internal sealed class RootContext : ContextBase, IDisposable
             throw new FileFormatException("Last used sector is invalid.");
 
         if (Version is Version.V4 && lastUsedSector.EndPosition < RangeLockSectorOffset)
-            Fat.TrySetValue(RangeLockSectorId, SectorType.Free);
+        {
+            if (Fat.TrySetValue(RangeLockSectorId, SectorType.Free))
+                Fat.Flush();
+        }
 
         Length = lastUsedSector.EndPosition;
         if (BaseStream.Length != Length)
